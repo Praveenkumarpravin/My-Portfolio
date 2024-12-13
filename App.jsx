3b10c51a4cbc875cc23cfc578adceef12ab36dfb
Praveen <131155311+Praveenkumarpravin@users.noreply.github.com>
2024-12-13 19:29:32 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import './../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+const App = () => {
+  return (
+    <>
+      <div className='container'>
+            i am container
+      </div>
+    </>
+  )
+}
+
+export default App;
